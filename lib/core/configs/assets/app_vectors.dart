33786f5fa72b2c6e749e20ef5b75b1edb702e1a6
blalfayed <fayedblal@yahoo.com>
2025-03-07 00:54:00 +0200

    first commit

---
+++ lib/core/configs/assets/app_vectors.dart
@@ -0,0 +1,5 @@
+class AppVectors {
+  static const basePath = 'assets/vectors/';
+  static const appLogo = '${basePath}logo$format';
+  static const format = '.svg';
+}
---
+++ lib/presentation/auth/pages/siginin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SigininPage extends StatelessWidget {
+  const SigininPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
